9eefad564ee6252e171cc05a0726a1d2d44df05c
Simon Haenel <info@simon-haenel.com>
2025-07-02 10:05:49 +0200

    [Build-Refactor] mainBuild.js als Einstiegspunkt für modularen Build-Prozess angelegt. (02.07.2025)

---
+++ build-process/mainBuild.js
@@ -0,0 +1,27 @@
+/**
+ * mainBuild.js
+ * Einstiegspunkt für den modularen Build-Prozess
+ * Erstellt: 02.07.2025
+ *
+ * TODO: Migration der Build-Logik aus scripts/build.js in Module
+ *
+ * Nach jedem Schritt: Dokumentation in docs/aenderungsprotokoll.md und Commit
+ */
+
+// Beispielhafter Modularisierungs-Start
+// Die eigentliche Logik wird in den nächsten Schritten aus scripts/build.js übernommen
+
+console.log('[mainBuild] Modularer Build-Prozess gestartet. (02.07.2025)');
+
+// TODO: Importiere und verwende Module aus build-process/modules/
+// z.B. const logger = require('./modules/logger');
+
+// TODO: Migration der Build-Logik aus scripts/build.js
+
+// TODO: Automatische Dokumentation und Performance-Logging
+
+// TODO: SEO-Checklisten-Integration
+
+// TODO: Terminal-Output-Logging
+
+// TODO: Nach jedem Schritt: git add & commit
