30dbb0636fae254209887d50de25f5068cb1f63f
Simon Haenel <info@simon-haenel.com>
2025-07-02 10:06:09 +0200

    [Build-Refactor] seoCheck.js Modul als Platzhalter für SEO-Checklisten und Validierung angelegt. (02.07.2025)

---
+++ build-process/modules/seoCheck.js
@@ -0,0 +1,16 @@
+/**
+ * seoCheck.js
+ * Modul für SEO-Checklisten und Validierung
+ * Erstellt: 02.07.2025
+ *
+ * TODO: Migration der SEO-Check-Logik aus scripts/build.js
+ */
+
+// Platzhalter für SEO-Check-Logik
+
+module.exports = {
+    check: (content, frontmatter) => {
+        // TODO: Implementiere SEO-Check-Logik
+        return [];
+    }
+};
