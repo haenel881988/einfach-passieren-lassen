8645e1fe3d3d29ebbecbef2de2b410db76f1788d
Simon Haenel <info@simon-haenel.com>
2025-07-02 10:06:00 +0200

    [Build-Refactor] logger.js Modul als Platzhalter für Terminal- und Build-Logging angelegt. (02.07.2025)

---
+++ build-process/modules/logger.js
@@ -0,0 +1,14 @@
+/**
+ * logger.js
+ * Modul für Terminal- und Build-Logging
+ * Erstellt: 02.07.2025
+ *
+ * TODO: Migration der Logger-Klasse aus scripts/build.js
+ */
+
+// Platzhalter für Logger-Logik
+
+module.exports = {
+    log: (...args) => console.log('[logger]', ...args),
+    // TODO: Weitere Methoden für File-Logging, Buffering, etc.
+};
