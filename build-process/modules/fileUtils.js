3c9e425328ac2f3c52badaa811c50b2fe5b2454c
Simon Haenel <info@simon-haenel.com>
2025-07-02 10:06:18 +0200

    [Build-Refactor] fileUtils.js Modul als Platzhalter für Dateioperationen angelegt. (02.07.2025)

---
+++ build-process/modules/fileUtils.js
@@ -0,0 +1,13 @@
+/**
+ * fileUtils.js
+ * Modul für Dateioperationen (lesen, schreiben, etc.)
+ * Erstellt: 02.07.2025
+ *
+ * TODO: Migration der Dateioperationen aus scripts/build.js
+ */
+
+// Platzhalter für Dateioperationen
+
+module.exports = {
+    // TODO: Implementiere Dateioperationen (read, write, scan, etc.)
+};
